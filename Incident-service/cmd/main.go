9aa9dc1f2e809e2b6beb9187d85ac68c0d53daae
snipedev <43064719+snipedev@users.noreply.github.com>
2026-04-19 17:46:06 +0530

    updated code for cors

--- Incident-service/cmd/main.go
+++ Incident-service/cmd/main.go
@@ -3,7 +3,9 @@ package main
 import (
 	"log"
 	"os"
+	"time"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 	"github.com/snipedev/ems/Incident-service/handlers"
@@ -20,6 +22,15 @@ func loadconfig() {
 
 func main() {
 	r := gin.Default()
+
+	r.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"http://localhost:3000"}, // frontend URL
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE"},
+		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		MaxAge:           12 * time.Hour,
+	}))
 
 	r.Use(gin.Recovery())
 	r.Use(gin.Logger())
